2341d9d72d7ac3944b29e63a649dcc33341342b5
zahidgx <al222310552@gmail.com>
2025-02-15 08:10:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         APOCO SI TILIN DEV
         </a>
       </header>
     </div>
